548d8118b31c017edd666482299fc5244cc2d4f6
ummar001 <m.ummar.uk@gmail.com>
2023-12-01 18:28:45 +0000

    init

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './components/Tictactoe/Tictactoe';
+
+function App() {
+  return (
+    <div >
+      <TicTacToe/>    
+    </div>
+  );
+}
+
+export default App;
